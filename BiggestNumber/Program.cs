048571fd4ca81d02cfee59c80aaab0bb1079a1c0
BalaBitbucket1 <balan7122001@gmail.com>
2023-11-09 09:10:02 +0530

    C# Program - To Find Biggest Number among three numbers.

---
+++ BiggestNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿// To find the largest of three numbers. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
+            int bigNum = 0;
+
+            Console.WriteLine("Input the three integer values: ");
+
+            for(int i = 0; i < 3; i++)
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < 3; i++)
+            {
+                if(bigNum < array[i])
+                {
+                    bigNum = array[i];
+                }
+            }
+            Console.WriteLine("Largest of three numbers: " + bigNum);
+        }
+    }
+}
+
+
+/*
+ Sample Output:
+
+Input the three integer values:
+45
+12
+-90
+Largest of three numbers: 45
+
+ */
