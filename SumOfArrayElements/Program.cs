6c373a557a88885bd2ee67414e45679b98e54345
BalaBitbucket1 <balan7122001@gmail.com>
2023-11-12 10:46:35 +0530

    Sum of array elements in C#

---
+++ SumOfArrayElements/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input the Array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+int sum = 0;
+Console.WriteLine("Input the {0} values:", size);
+
+for (int i = 0; i < size; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    sum += arr[i];
+}
+
+Console.WriteLine("Sum of the array elements are: {0}", sum);
+
+/*  
+ 
+ Sample Output:
+
+Input the Array size: 5
+Input the 5 values:
+1
+2
+3
+4
+5
+Sum of the array elements are: 15
+
+ */
+
+
