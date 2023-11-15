59bdd1ca34d7fce6a7fe0504edaac3417d50a7a0
BalaBitbucket1 <balan7122001@gmail.com>
2023-11-15 13:45:00 +0530

    Foreach in C#

---
+++ SumOfArrayElementsUsing_ForEach/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input the array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+Console.WriteLine("Enter the {0} Elements:", size);
+for (int i = 0; i < size; i++)
+{   
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int sum = 0;
+foreach (int i in arr)
+    sum += i;
+
+Console.WriteLine("Sum is: {0}", sum);
+
+/*    
+Sample Output:
+
+Input the array size: 10
+Enter the 10 Elements:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Sum is: 55
+ 
+ */
