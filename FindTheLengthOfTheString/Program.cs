199f8e8dc842a1cea51efbeb3f8c054d67c66e4f
BalaBitbucket1 <balan7122001@gmail.com>
2023-11-16 08:57:35 +0530

    16-11-23 To Find The Length of the string

---
+++ FindTheLengthOfTheString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheLengthOfTheString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the string: ");
+            string Str = Console.ReadLine();
+            Console.WriteLine("Given String is: {0}", Str);
+            Console.WriteLine("Length of the string is: {0}", Str.Length);
+        }
+    }
+}
+
+/* 
+ Sample Output:
+
+Input the string:
+A string in C# is actually an object.
+Given String is: A string in C# is actually an object.
+Length of the string is: 37
+
+ */
