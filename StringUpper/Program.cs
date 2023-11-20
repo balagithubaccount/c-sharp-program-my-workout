269d058f1a70604b8091769a70293adc6bb62884
BalaBitbucket1 <balan7122001@gmail.com>
2023-11-20 09:02:14 +0530

    C# Program, String to UpperCase

---
+++ StringUpper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+       Console.Write("Input the string in lowercase: ");
+       string str;
+       str = Console.ReadLine();
+       Console.WriteLine("Given string(in Upper): {0}", str.ToUpper());
+    }
+  }
+}
+/*
+Sample Output:
+
+Input the string in lowercase: c# program
+Given string(in Upper): C# PROGRAM
+
+*/
