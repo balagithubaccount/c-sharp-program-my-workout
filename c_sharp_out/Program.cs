c4cd045473aee21577187aa5c33108d7f92f4c8c
BalaBitbucket1 <balan7122001@gmail.com>
2023-11-23 22:30:08 +0530

    C# out keyword

---
+++ c_sharp_out/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CallByRef_C_
+{
+    internal class Program
+    {
+        static void Show(out int num)
+        {
+            int value = 10;
+            num = value;
+        }
+        static void Main(string[] args)
+        {
+            int n = 100;
+
+            Console.WriteLine("Before Calling: " + n);
+            Show(out n);
+            Console.WriteLine("After Calling: " + n);
+        }
+    }
+}
+
+/*
+Sample Output:
+
+Before Calling: 100
+After Calling: 10
+
+*/
