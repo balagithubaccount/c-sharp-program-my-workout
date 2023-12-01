d1d787a6227ef7f0dfd33a55577fb1c5f46da426
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-01 21:46:26 +0530

    C# Dual Loop Using To Print Square Patter.

---
+++ SquarePattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows: ");
+        int no_of_row = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < no_of_row; i++)
+        {
+            for (int j = 0; j < no_of_row; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
+
+/*
+Sample Output:
+
+Enter the number of rows: 5
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+*/
