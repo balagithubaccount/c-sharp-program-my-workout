e44ccbee288594c038478618f30b33b6e2832e9b
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-03 16:26:20 +0530

    To print right angle triangle using * symbol.

---
+++ RightAngleTriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Input the number of row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < row; i++)
+{
+    for(int j = 0; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.Write("\n");
+}
+/*
+Sample Output:
+
+Input the number of row: 7
+*      
+**     
+***    
+****   
+*****  
+****** 
+*******
+*/
