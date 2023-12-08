b848d41e125ab3e265d2e7359eeb4d24796cd858
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-08 11:12:58 +0530

    C# Patternt Printing - Triangle using number

---
+++ Pattern_Triangle_Num/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Triangle_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 6;
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
+
+/*
+Sample Output:
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+
+*/
