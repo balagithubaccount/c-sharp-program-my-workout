f67f79f68efa7b91094eed8901ece398c25728bf
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-17 16:35:36 +0530

    C# math function

---
+++ c_sharp_math_function_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace c_sharp_math_function_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Math Function:");
+            Console.WriteLine("Square Root of 144 is: {0}", Math.Sqrt(144));
+            Console.WriteLine("Round of 912.3254 is: {0}", Math.Round(912.3254));
+        }
+    }
+}
+
+/*
+Sample Output:
+
+C# Math Function:
+Square Root of 144 is: 12
+Round of 912.3254 is: 912
+
+*/
