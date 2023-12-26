c24c913b73c1b165fa65ee7a2a9aa88004346d9e
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-26 14:10:47 +0530

    Conditional ternary operator Buzzle

---
+++ c-sharp-conditional-program/ternary_operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class First
+{
+    public static int Main()
+    {
+        int a = 5;
+        int b = 10;
+        int result;
+
+        result = (a > 0) ? ((b > 0) ? (a = 10) : (a = 5)) : a;
+
+        Console.WriteLine("Result: {0}", result);
+
+        // Return a value indicating the program executed successfully
+        return 0;
+    }
+}
