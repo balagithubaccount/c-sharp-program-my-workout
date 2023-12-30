742379dc1378d48e7b038d6d8a5e0f44aaa931b9
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-30 22:19:21 +0530

    C# Type and typeof operator

--- TypeOperator/Program.cs
+++ TypeOperator/Program.cs
@@ -4,23 +4,40 @@ namespace MyApplication
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int myInt = 10;
-            double myDouble = 5.25;
-            bool myBool = true;
+        static Type a = typeof(double);
 
-            Console.WriteLine(myInt.GetType());
-            Console.WriteLine(myDouble.GetType());
-            Console.WriteLine(myBool.GetType());
+        // Main method
+        static void Main()
+        {
+            // Display the type of a
+            Console.WriteLine(a);
+
+            // Display the value type
+            Console.WriteLine(typeof(int));
+
+            // Display the class type
+            Console.WriteLine(typeof(Array));
+
+            // Display the value type
+            Console.WriteLine(typeof(char));
+
+            // Display the array reference type
+            Console.WriteLine(typeof(int[]));
+
+            // Display the string reference type
+            Console.WriteLine(typeof(string));
         }
     }
 }
+
 /*
 Sample Output:
 
+System.Double
 System.Int32
-System.Double 
-System.Boolean
+System.Array
+System.Char
+System.Int32[]
+System.String
 
 */
