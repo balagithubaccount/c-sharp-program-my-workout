1800c53f92daefb6179fb758c089180eccdb30c0
BalaBitbucket1 <balan7122001@gmail.com>
2023-12-31 21:24:27 +0530

    C# GetType method.

---
+++ C_Sharp_GetTypeMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine(myInt.GetType());
+            Console.WriteLine(myDouble.GetType());
+            Console.WriteLine(myBool.GetType());
+        }
+    }
+}
+/*
+Sample Output:
+
+System.Int32
+System.Double 
+System.Boolean
+*/
