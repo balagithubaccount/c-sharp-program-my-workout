e4105729bc9768dcb57c52039f5b0fbecd8632f1
BalaBitbucket1 <balan7122001@gmail.com>
2024-01-01 11:43:15 +0530

    C# Even or Odd

---
+++ c-sharp-conditional-program/Given_Num_Even_or_Odd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static Type a = typeof(double);
+
+        // Main method
+        static void Main() 
+        { 
+            int givenNum;
+            Console.Write("Enter the integer value: ");
+            givenNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Given Number {0} is ", givenNum);
+            if(givenNum % 2 == 0)
+            Console.WriteLine("Even.");
+            else 
+            Console.WriteLine("Odd");
+        }
+    }
+}
+
+/*
+Sample Output:
+
+Enter the integer value: 53
+Given Number 53 is Odd
+*/
