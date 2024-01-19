f5da6a211097f517d532e2a7215a8aeb4fd13f8c
BalaBitbucket1 <balan7122001@gmail.com>
2024-01-19 09:08:14 +0530

    Concadinate two string variable values.

--- Concadinate_two_variable/Program.cs
+++ Concadinate_two_variable/Program.cs
@@ -6,19 +6,27 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
-      string firstName = "John ";
-      string lastName = "Doe";
-      string fullName = firstName + lastName;
-      Console.WriteLine("First Name: " + firstName);
-      Console.WriteLine("Last Name: " + lastName);
-      Console.WriteLine("Full Name: " + fullName);
+      string firstName, lastName, fullName;
+      Console.Write("Enter your first name: ");
+      firstName = Console.ReadLine();
+
+      Console.Write("Enter your second name: ");
+      lastName = Console.ReadLine();
+
+      fullName = firstName + " " + lastName;
+
+      Console.WriteLine("First Name is: " + firstName);
+      Console.WriteLine("Last Name is: " + lastName);
+      Console.WriteLine("Full Name is: " + fullName);
     }
   }
 }
 /*
 Sample Output:
 
-First Name: John 
-Last Name: Doe     
-Full Name: John Doe
+Enter your first name: John
+Enter your second name: Wick
+First Name is: John    
+Last Name is: Wick     
+Full Name is: John Wick
 */
